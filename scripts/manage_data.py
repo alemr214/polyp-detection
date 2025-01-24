0fd06a74bf92263ed6a835a78d3c88c217b1ea79
alemr214 <ale.mr0214@gmail.com>
2025-01-24 01:52:25 -0600

    Add function to copy images to a new directory with error handling

--- scripts/manage_data.py
+++ scripts/manage_data.py
@@ -13,3 +13,22 @@ def create_dir(output_path: str) -> None:
             os.makedirs(output_path)
     except OSError:
         print(f"Error: Creating directory. {output_path}")
+
+
+def copy_images(source_path: str, output_path: str, image_ext: str) -> None:
+    """
+    Move images to a new directory
+
+    Args:
+        source_path (str): Directory path to images
+        image_ext (str): Image extension
+    """
+    create_dir(output_path)  # Create output directory
+    for file in os.listdir(source_path):
+        if file.endswith(image_ext):
+            source_path_ = os.path.join(source_path, file)
+            output_path_ = os.path.join(output_path, file)
+            try:
+                shutil.copy(source_path_, output_path_)
+            except Exception as e:
+                print(f"Error: Moving file. {source_path} {file}: {e}")
