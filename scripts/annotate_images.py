a76af74b60dcded2752d75ab3af047160c7f7948
alemr214 <ale.mr0214@gmail.com>
2025-01-24 02:30:30 -0600

    Refactor: Remove local detect_imgs function and import from manage_data module

--- scripts/annotate_images.py
+++ scripts/annotate_images.py
@@ -1,31 +1,9 @@
 import os
 import torch
-import numpy as np
 from torchvision.io import read_image, ImageReadMode
-from .manage_data import create_dir
+from .manage_data import create_dir, detect_imgs
 
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-
-
-# Detect images in a folder an return a sorted list of images path
-def detect_imgs(infolder: str, ext: str = ".png") -> np.ndarray:
-    """
-    Returns a sorted list of images path with some extension (.png as default) in a directory path
-
-    Args:
-        infolder (str): Images path
-        ext (str, optional): Extension of images. Defaults to ".png".
-
-    Returns:
-        np.ndarray: Sorted list of image paths
-    """
-    items = os.listdir(infolder)
-    flist = [
-        os.path.join(infolder, names)
-        for names in items
-        if names.endswith(ext) or names.endswith(ext.upper())
-    ]
-    return np.sort(flist)
 
 
 # Save bounding box in YOLO format
