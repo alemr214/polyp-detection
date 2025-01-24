a2cf641fdd9a0b70678f9601a423fb6980cb29ad
alemr214 <ale.mr0214@gmail.com>
2025-01-24 00:51:34 -0600

    Add function to calculate bounding box coordinates from mask object

--- scripts/annotate_images.py
+++ scripts/annotate_images.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import numpy as np
+from torchvision.io import read_image, ImageReadMode
 
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
@@ -53,3 +54,24 @@ def save_bbox(txt_path: str, line: str) -> None:
     txt_path = txt_path + ".txt"
     with open(txt_path, "w") as myfile:
         myfile.write(line + "\n")
+
+
+# Calculate bounding box coordinates
+def valRect(coord: torch.Tensor) -> list:
+    """
+    Calculate bounding box coordinates from a list of coordinates (x, y) of a mask object
+
+    Args:
+        coord (np.ndarray): List of coordinates (x, y) of a mask object in a binary mask with shape (n, 1, 2) where:
+                            n: number of pixels in the mask object
+                            1: number of channels
+                            2: x and y coordinates
+
+    Returns:
+        list: Returns a list with the coordinates of the bounding box [xmin, ymin, xmax, ymax]
+    """
+    xmin = torch.min(coord[:, 0]) + 1
+    ymin = torch.min(coord[:, 1]) + 1
+    xmax = torch.max(coord[:, 0]) + 1
+    ymax = torch.max(coord[:, 1]) + 1
+    return [xmin.item(), ymin.item(), xmax.item(), ymax.item()]
