ce41a44b0b7980f368ca255669917a25c5423dc4
alemr214 <ale.mr0214@gmail.com>
2025-01-29 14:25:03 -0600

    Enhance main.py to support additional mask processing and improve data management workflow

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from scripts.annotate_images import process_images
 from scripts.manage_data import copy_images, rename_files, split_data
 
 if __name__ == "":
+    # BASE PATH
+    BASE_PATH = ""
+    NAME_DATASET = ""
+
+    # Data paths
     IMAGES_FOLDER = ""  # To copy images from
     IMAGES_EXT = ""  # Image extension
     MASKS_FOLDER = ""  # To process masks from images
     MASKS_EXT = ""  # Mask extension
     OUTPUT_IMAGES_FOLDER = ""  # To save the copy images
     OUTPUT_LABELS_FOLDER = ""  # To save the .txt bbox annotations from masks
+    OUTPUT_MASKS_FOLDER = ""
     CLASS_INDEX = 0  # Class index to save the annotations
     PREFIX = ""  # Prefix to rename files ("images")
 
     copy_images(IMAGES_FOLDER, OUTPUT_IMAGES_FOLDER, IMAGES_EXT)
+    copy_images(MASKS_FOLDER, OUTPUT_MASKS_FOLDER, MASKS_EXT)
 
     process_images(
         OUTPUT_IMAGES_FOLDER,
@@ -24,5 +31,6 @@ if __name__ == "":
 
     rename_files(OUTPUT_IMAGES_FOLDER, PREFIX, IMAGES_EXT)
     rename_files(OUTPUT_LABELS_FOLDER, PREFIX, "")  # txt for labels
+    rename_files(OUTPUT_MASKS_FOLDER, PREFIX, MASKS_EXT)
 
     split_data(OUTPUT_IMAGES_FOLDER, OUTPUT_LABELS_FOLDER)
