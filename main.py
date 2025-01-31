0740896d389c94fac17316ad9ed3ca62e7ad8ca8
alemr214 <ale.mr0214@gmail.com>
2025-01-31 16:07:57 -0600

    Enhance main.py to include YAML file creation and improve dataset configuration paths

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from scripts.annotate_images import process_images
-from scripts.manage_data import copy_images, rename_files, split_data
+from scripts.manage_data import (
+    copy_images,
+    rename_files,
+    split_data,
+)
+from scripts.prepare_models import create_yaml_file
 
 if __name__ == "__main__":
-    # Configuration
+    # Configuration paths to clean the dataset
     BASE_PATH_RAW = ""
     BASE_PATH_CLEAN = ""
+    BASE_PATH_YAML = "./configs"
     NAME_DATASET = ""
 
     # Data paths
@@ -35,7 +41,18 @@ if __name__ == "__main__":
     )
 
     rename_files(OUTPUT_IMAGES_FOLDER, PREFIX, IMAGES_EXT)
-    rename_files(OUTPUT_LABELS_FOLDER, PREFIX, "")  # txt for labels
+    rename_files(OUTPUT_LABELS_FOLDER, PREFIX, "")  # .txt for labels
     rename_files(OUTPUT_MASKS_FOLDER, PREFIX, MASKS_EXT)
 
     split_data(OUTPUT_IMAGES_FOLDER, OUTPUT_LABELS_FOLDER)
+
+    # Create the YAML file
+    create_yaml_file(
+        BASE_PATH_CLEAN,
+        f"{NAME_DATASET}/images/train",
+        f"{NAME_DATASET}/images/val",
+        f"{NAME_DATASET}/images/test",
+        CLASS_INDEX,
+        [""],
+        BASE_PATH_YAML,
+    )
