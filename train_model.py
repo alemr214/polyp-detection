691eea68d06af078d2d42d4ebdef559d9bd46324
alemr214 <ale.mr0214@gmail.com>
2025-02-12 15:38:15 -0600

    Refactor training script to dynamically select device and export best model in ONNX format

--- train_model.py
+++ train_model.py
@@ -1,14 +1,37 @@
 from ultralytics import YOLO
+import torch
 
-model = YOLO("runs/trains/yolo11n.pt")
 
-results = model.train(
-    data="configs/sessile_main_kvasir_seg/dataset.yaml",
-    epochs=10,
-    imgsz=640,
-    batch=-1,
-    device="mps",
-    name="sessile_main_kvasir_seg",
-    project="runs/trains",
-    exist_ok=True,
-)
+if __name__ == "__main__":
+    BASE_PATH_YAML = "configs"
+    NAME_DATASET = "sessile_main_kvasir_seg"
+    BASE_PATH_MODEL = "runs/trains"
+
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+
+    model = YOLO(f"{BASE_PATH_MODEL}/{NAME_DATASET}/yolo11n.pt")
+
+    results = model.train(
+        data=f"{BASE_PATH_YAML}/{NAME_DATASET}/dataset.yaml",
+        epochs=10,
+        imgsz=640,
+        batch=-1,
+        device=device,
+        name=NAME_DATASET,
+        project=BASE_PATH_MODEL,
+        exist_ok=True,
+    )
+
+    best_model = YOLO(f"{BASE_PATH_MODEL}/{NAME_DATASET}/weights/best.pt")
+
+    best_model.export(
+        format="onnx",
+        opset=12,
+        device=device,
+    )
