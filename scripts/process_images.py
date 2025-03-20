1c124139033668d7dccb34ac16268b3fb24ad7bc
alemr214 <ale.mr0214@gmail.com>
2025-03-20 11:41:40 -0600

    Refactor image processing script to use OpenCV for object detection and bounding box annotation; streamline functions and improve code readability.

--- scripts/process_images.py
+++ scripts/process_images.py
@@ -1,143 +1,126 @@
 import os
-import torch
-from torchvision.io import read_image, ImageReadMode
-from torchvision.utils import draw_bounding_boxes
-from torchvision.transforms.functional import to_pil_image
-from .manage_data import create_dir, detect_imgs
-
-# Set device to CUDA if available or MPS for better performance in a macbook with M1 chip or CPU
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
-)
+import cv2
+from .manage_data import detect_files, create_dir
 
 
-# Save bounding box in YOLO format
 def save_bbox(txt_path: str, line_to_write: str) -> None:
     """
-    Save bounding box in YOLO format in a txt file with the specified path
+    Save bounding box in a txt file with specified path
 
     Args:
-        txt_path (str): Path to save the txt file
-        line_to_write (str): Parameters of the bounding box in YOLO format to write in a file
+        txt_path (str): Path to save the file
+        line_to_write (str): Parameters of the bounding box to write in a file
     """
     txt_path = txt_path + ".txt"
-    with open(txt_path, "w") as myfile:
-        myfile.write(line_to_write + "\n")
+    with open(txt_path, "w") as my_file:
+        my_file.write(line_to_write + "\n")
 
 
-# Calculate bounding box coordinates
-def valRect(coord: torch.Tensor) -> list:
+def detect_object(mask: str) -> list | None:
     """
-    Calculate bounding box coordinates from a list of coordinates (x, y) of a mask object
+    Detect objects in a binary mask and return the coordinates for every object detected in a list of tuples.
 
     Args:
-        coord (np.ndarray): List of coordinates (x, y) of a mask object in a binary mask with shape (n, 1, 2) where:
-                            n: number of pixels in the mask object
-                            1: number of channels
-                            2: x and y coordinates
+        mask (str): Path to the binary mask image.
 
     Returns:
-        list: Returns a list with the coordinates of the bounding box [xmin, ymin, xmax, ymax]
+        list | None: List of tuples with the coordinates of the objects detected in the mask.
     """
-    # Coordinates of the bounding box in the format [xmin, ymin, xmax, ymax] with index 1
-    xmin = torch.min(coord[:, 0]) + 1
-    ymin = torch.min(coord[:, 1]) + 1
-    xmax = torch.max(coord[:, 0]) + 1
-    ymax = torch.max(coord[:, 1]) + 1
-    return [xmin.item(), ymin.item(), xmax.item(), ymax.item()]
+    mask_image = cv2.imread(mask, cv2.IMREAD_GRAYSCALE)
+    num_labels, _, stats, _ = cv2.connectedComponentsWithStats(mask_image)
+    objects_coordiantes = []
+    for i in range(1, num_labels):
+        x, y, w, h = (
+            stats[i][cv2.CC_STAT_LEFT],
+            stats[i][cv2.CC_STAT_TOP],
+            stats[i][cv2.CC_STAT_WIDTH],
+            stats[i][cv2.CC_STAT_HEIGHT],
+        )
+        objects_coordiantes.append((x, y, x + w, y + h))
+    return objects_coordiantes
 
 
-# Convert bounding box to YOLO format
-def yolo_format(class_index: int, coord: torch.Tensor, width: int, height: int) -> str:
+def normalize_coordiantes(coordinates: list) -> list:
     """
-    Convert bounding box coordinates to YOLO format using the coordinates calculated from the mask object in the valRect function
+    Normalize the coordinates to get the center, width, and height of objects detected in a mask.
 
     Args:
-        class_index (int): Index of the class
-        coord (np.ndarray): List of coordinates (x, y) of a mask object in a binary mask with shape (n, 1, 2)
-        width (int): With of the all image
-        height (int): Height of the all image
+        coordinates (list): List of tuples with the coordinates of the objects detected in the mask.
 
     Returns:
-        str: Returns a string with the bounding box coordinates in YOLO format rounded to 6 decimal places
+        list: List of tuples with the normalized coordinates of the objects detected in the mask.
     """
-    [xmin, ymin, xmax, ymax] = valRect(coord)
-    x_center = (xmin + xmax) / (2.0 * width)
-    y_center = (ymin + ymax) / (2.0 * height)
-    x_width = (xmax - xmin) / width
-    y_height = (ymax - ymin) / height
-    return f"{class_index} {x_center:.6f} {y_center:.6f} {x_width:.6f} {y_height:.6f}"
+    coordinates_normalized = []
+    for coordiante in coordinates:
+        x1, y1, x2, y2 = coordiante
+        x_center = (x1 + x2) / 2
+        y_center = (y1 + y2) / 2
+        width = x2 - x1
+        height = y2 - y1
+        coordinates_normalized.append((x_center, y_center, width, height))
+    return coordinates_normalized
 
 
-# Process a binary mask to get the coordinates of the object
-def get_mask_coordinates(mask: torch.Tensor) -> torch.Tensor:
+def yolo_format(
+    class_index: int, norm_coordinates: list, image_width: int, image_height: int
+) -> list:
     """
-    Get the coordinates of the mask object obtaining the non-zero coordinates and reversing the order
+    Get the YOLO format of the coordinates of the objects detected in a mask.
 
     Args:
-        mask (torch.Tensor): Binary mask with shape (1, H, W)
+        class_index (int): Index of the class.
+        norm_coordinates (list): List of tuples with the normalized coordinates of the objects detected in the mask.
+        image_width (int): Width of the image.
+        image_height (int): Height of the image.
 
     Returns:
-        torch.Tensor: Returns the coordinates of the mask object in the format (n, 2)
+        list: List of strings with the YOLO format of the coordinates of the objects detected in the mask.
     """
-    return torch.nonzero(mask.squeeze(), as_tuple=False)[:, [1, 0]]
+    yolo_format = []
+    for coord in norm_coordinates:
+        x_center, y_center, width, height = coord
+        x_center /= image_width
+        y_center /= image_height
+        width /= image_width
+        height /= image_height
+        yolo_format.append(f"{class_index} {x_center} {y_center} {width} {height}")
+    return yolo_format
 
 
-# Main function to call and process images
 def annotate_images(
-    image_folder: str,
-    mask_folder: str,
-    output_folder: str,
-    class_index: int = 0,  # Only allows one class
+    images_path: str, masks_path: str, output_labels_path: str, class_index: int = 0
 ) -> None:
     """
-    Call all necessary functions to create the directory, get the coordinates, calculate the bounding box in YOLO format, and save the .txt file
+    Annotate images with the bounding boxes of the objects detected in the masks and save the labels in a txt file.
 
     Args:
-        image_folder (str): Directory path to images
-        mask_folder (str): Directory path to masks
-        output_folder (str): Directory path to save the .txt files
-        class_index (int, optional): Index object classes. Defaults to 0.
+        images_path (str): Path to the images.
+        masks_path (str): Path to the masks.
+        output_labels_path (str): Path to save the labels.
+        class_index (int, optional): Index of the class. Defaults to 0.
     """
-    create_dir(output_folder)  # Create output directory
-
-    images = detect_imgs(image_folder)
-    masks = detect_imgs(mask_folder)
-
-    # Iterates and uncompress every path of images and masks
-    for img_path, mask_path in zip(images, masks):
-        image = read_image(img_path).to(device)
-        # Read mask in grayscale
-        mask = read_image(mask_path, mode=ImageReadMode.GRAY).to(device)
-
-        # Get image shape
-        h, w = image.shape[1], image.shape[2]
-
-        # Find non-zero coordinates in the mask and reverse the order
-        mask_coordinates = get_mask_coordinates(mask)
-
-        # If no objects found in the mask, skip the image
-        if mask_coordinates.size(0) == 0:
-            print(f"No objects found in mask: {mask_path}")
-            continue
-
-        # Convert mask coordinates to YOLO format
-        yolo_line = yolo_format(class_index, mask_coordinates, w, h)
-
-        # Save bounding box in YOLO format
-        base_name = os.path.splitext(os.path.basename(img_path))[0]
-        output_txt_path = os.path.join(output_folder, base_name)
-        save_bbox(output_txt_path, yolo_line)
-
-        print(f"Processed: {img_path} -> {output_txt_path}")
+    create_dir(output_labels_path)
+    images = detect_files(images_path, [".png", ".jpg", ".tif"])
+    masks = detect_files(masks_path, [".png", ".jpg", ".tif"])
+    for image, mask in zip(images, masks):
+        image_read = cv2.imread(image)
+        objects_coordinates = detect_object(mask)
+        objects_coordinates = normalize_coordiantes(objects_coordinates)
+        yolo_labels = yolo_format(
+            class_index,
+            objects_coordinates,
+            image_read.shape[1],
+            image_read.shape[0],
+        )
+        base_name = os.path.splitext(os.path.basename(image))[0]
+        output_txt_path = os.path.join(output_labels_path, base_name)
+        save_bbox(output_txt_path, "\n".join(yolo_labels))
 
 
 def draw_bounding_boxes_on_images(
-    images_path: str, masks_path: str, output_path: str, color: str = "blue"
+    images_path: str,
+    masks_path: str,
+    output_bbox_images: str,
 ) -> None:
     """
     Draw bounding boxes on images using the coordinates obtained from the mask object
@@ -145,34 +128,19 @@ def draw_bounding_boxes_on_images(
     Args:
         images_path (str): Path of images
         masks_path (str): Path of masks
-        output_path (str): Path to save the images with bounding boxes drawn
-        color (str, optional): Color of the box drawn. Defaults to "blue".
+        output_bbox_images (str): Path to save the images with bounding boxes
     """
-    create_dir(output_path)
+    create_dir(output_bbox_images)
 
-    images = detect_imgs(images_path)
-    masks = detect_imgs(masks_path)
-
-    for img_path, mask_path in zip(images, masks):
-        image = read_image(img_path).to(torch.uint8).to(device)
-        mask = read_image(mask_path, mode=ImageReadMode.GRAY).to(device)
-
-        name_image, ext_image = os.path.splitext(os.path.basename(img_path))
-
-        mask_positives_pixels = get_mask_coordinates(mask)
-
-        [xmin, ymin, xmax, ymax] = valRect(mask_positives_pixels)
-
-        bbox_coordinates = torch.tensor(
-            [[xmin, ymin, xmax, ymax]], dtype=torch.int, device=device
-        )
-
-        image_with_boxes = draw_bounding_boxes(
-            image, bbox_coordinates, width=3, colors=[color]
-        )
-
-        image_with_boxes_pil = to_pil_image(image_with_boxes)
-
-        image_with_boxes_pil.save(
-            os.path.join(output_path, f"{name_image}_bbox{ext_image}")
-        )
+    images = detect_files(images_path, [".png", ".jpg", ".tif"])
+    masks = detect_files(masks_path, [".png", ".jpg", ".tif"])
+    for image_path, mask_path in zip(images, masks):
+        image = cv2.imread(image_path)
+        object_coordinates = detect_object(mask_path)
+        for coord in object_coordinates:
+            x1, y1, x2, y2 = coord
+            cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 4)
+            base_name = os.path.splitext(os.path.basename(image_path))[0]
+            ext_type = os.path.splitext(os.path.basename(image_path))[1]
+            output_image_path = os.path.join(output_bbox_images, base_name)
+            cv2.imwrite(output_image_path + ext_type, image)
